5de45ee080b9879ae2485baf76e885f43cd35948
stellawltr <119009232+stellawltr@users.noreply.github.com>
2023-11-14 22:33:28 +0100

    moment js implemented, real time data can be fetched now

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,38 @@
+function updateFrankfurtTime() {
+  // Frankfurt
+  let FrankfurtElement = document.querySelector("#frankfurt");
+  let FrankfurtDateElement = FrankfurtElement.querySelector(".date");
+  let FrankfurtTimeElement = FrankfurtElement.querySelector(".time");
+  let FrankfurtTime = moment().tz("Europe/Berlin");
 
+  FrankfurtDateElement.innerHTML = FrankfurtTime.format("MMMM Do YYYY");
+  FrankfurtTimeElement.innerHTML = FrankfurtTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+
+setInterval(updateFrankfurtTime, 1000);
+
+function updateTokyoTime() {
+  // Tokyo
+  let TokyoElement = document.querySelector("#tokyo");
+  let TokyoDateElement = TokyoElement.querySelector(".date");
+  let TokyoTimeElement = TokyoElement.querySelector(".time");
+  let TokyoTime = moment().tz("Asia/Tokyo");
+  TokyoDateElement.innerHTML = TokyoTime.format("MMMM Do YYYY");
+  TokyoTimeElement.innerHTML = TokyoTime.format("h:mm:ss [<small>]A[</small>]");
+}
+setInterval(updateTokyoTime, 1000);
+
+function updateAucklandTime() {
+  // Auckland
+  let AucklandElement = document.querySelector("#aucklamd");
+  let AucklandDateElement = AucklandElement.querySelector(".date");
+  let AucklandTimeElement = AucklandElement.querySelector(".time");
+  let AucklandTime = moment().tz("Pacific/Auckland");
+  AucklandDateElement.innerHTML = AucklandTime.format("MMMM Do YYYY");
+  AucklandTimeElement.innerHTML = AucklandTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+setInterval(updateAucklandTime, 1000);
